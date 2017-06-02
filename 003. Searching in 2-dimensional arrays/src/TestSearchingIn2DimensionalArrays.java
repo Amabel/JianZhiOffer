cea1c9773de2630170a8e0bf9ae05af51a97eb83
Amabel <luoweibinb@gmail.com>
2017-06-02 20:57:55 +0900

    update .gitignore & add JUnit

---
+++ 003. Searching in 2-dimensional arrays/src/TestSearchingIn2DimensionalArrays.java
@@ -0,0 +1,38 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author  Weibin Luo
+ * @version Created on 2017/06/02 15:58:52
+ */
+public class TestSearchingIn2DimensionalArrays {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testFind() {
+// 		the 2D arrays
+//		 1  2  8  9
+//       2  4  9  12
+//       4  7  10 13
+//       6  8  11 15
+		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 1));
+		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 9));
+		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 7));
+		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 6));
+		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 15));
+		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 10));
+		
+		assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 0));
+		assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, -1));
+		assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 2147483647));
+		assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, -100000000));
+	}
+
+}
