82c5dca02c3a823a6bf58def168caa086ab30809
Amabel <luoweibinb@gmail.com>
2017-06-02 20:56:19 +0900

    add test cases

--- 003. Searching in 2-dimensional arrays/src/SearchingIn2DimensionalArrays.java
+++ 003. Searching in 2-dimensional arrays/src/SearchingIn2DimensionalArrays.java
@@ -1,20 +1,51 @@
 public class SearchingIn2DimensionalArrays {
+	
+	public static int[][] array = {
+            {1, 2, 8, 9},
+            {2, 4, 9, 12},
+            {4, 7, 10, 13},
+            {6, 8, 11, 15}
+	};
+//	public static int[][] array = null;
+	
     public static void main(String[] args) {
-        int target = 7;
-        int[][] array = {
-                            {1, 2, 8, 9},
-                            {2, 4, 9, 12},
-                            {4, 7, 10, 13},
-                            {6, 8, 11, 15}
-                        };
-        boolean isFound = new SearchingIn2DimensionalArrays().find(array, target);
+        int target = 2147483647;
+        boolean isFound = find(array, target);
 
         System.out.println(target + " is found in array: " + isFound);
 
 
     }
 
-    boolean find(int[][] array, int target) {
-        return false;
+    public static boolean find(int[][] array, int target) {
+    	// check if the array is null
+    	if (array == null) {
+    		throw new IllegalArgumentException("array is null!");
+    	}
+    	// get the rows and cols of the array
+    	int rows = array.length;
+    	int cols = array[0].length;
+    	// check if the array is empty
+    	if (rows == 0 || cols == 0) {
+    		throw new IllegalArgumentException("array is empty!");
+    	}
+    	
+    	// start from top of the right (or left of the bottom)
+    	int i = 0;
+    	int j = cols - 1;
+    	boolean isFound = false;
+    	do {
+    		int n = array[i][j];
+    		if (target == n) {
+    			isFound = true;
+    			break;
+    		} else if (target > n){
+    			i ++;
+    		} else {
+    			j --;
+    		}
+    	} while (i <= rows - 1 && j >= 0);
+
+        return isFound;
     }
 }
