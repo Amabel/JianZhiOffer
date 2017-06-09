54c05ee105ec61034386236ed27582a1405fbba9
Amabel <luoweibinb@gmail.com>
2017-06-09 16:59:34 +0900

    reformat

--- 003. Searching in 2-dimensional arrays/src/test/TestSearchingIn2DimensionalArrays.java
+++ 003. Searching in 2-dimensional arrays/src/test/TestSearchingIn2DimensionalArrays.java
@@ -14,30 +14,30 @@ import solution.SearchingIn2DimensionalArrays;
  */
 public class TestSearchingIn2DimensionalArrays {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@Test
-	public void testFind() {
-// 		the 2D arrays
-//		 1  2  8  9
+    @Test
+    public void testFind() {
+//      the 2D arrays
+//       1  2  8  9
 //       2  4  9  12
 //       4  7  10 13
 //       6  8  11 15
-		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 1));
-		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 9));
-		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 7));
-		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 6));
-		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 15));
-		assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 10));
-		
-		assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 0));
-		assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, -1));
-		assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 2147483647));
-		assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, -100000000));
-		assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 3));
-		assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 5));
-	}
+        assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 1));
+        assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 9));
+        assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 7));
+        assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 6));
+        assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 15));
+        assertTrue(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 10));
+
+        assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 0));
+        assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, -1));
+        assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 2147483647));
+        assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, -100000000));
+        assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 3));
+        assertFalse(SearchingIn2DimensionalArrays.find(SearchingIn2DimensionalArrays.array, 5));
+    }
 
 }
